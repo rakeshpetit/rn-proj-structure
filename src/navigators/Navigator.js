79297d3e579772bd2c17aa00674484efc0db003a
Rakesh <rakeshpetit@gmail.com>
2019-10-10 17:56:13 +0100

    Using variables for scss

--- src/navigators/Navigator.js
+++ src/navigators/Navigator.js
@@ -1,8 +1,6 @@
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 import {Home, Profile} from '../screens';
-// import Home from './Home';
-// import Profile from './Profile';
 
 const AppNavigator = createStackNavigator({
   Home: {
